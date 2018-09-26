4ee09666fe35ae1a0aeca533db4e0d30138438c4
Valeryshchurik <Valeryschurik@gmail.com>
2018-09-26 17:33:38 +0300

    Add files via upload

--- lab1.2.asm
+++ lab1.2.asm
@@ -1,54 +1,62 @@
-.MODEL TINY
-.DATA
-	CONST16 DB 16
-	HEXSYM DB '0123456789ABCDEF'
-	CODSYM DB 0
-.CODE
-.STARTUP
-	MOV CX,256
-	
-PRINT_SYMBOL:
-	PUSH CX
-	MOV AX,40H
-	MOV ES,AX
-	MOV BH,ES:[62H]
-	MOV AH,0AH
-	MOV AL,CODSYM
-	MOV CX,1
-	INT 10H
-	
-	MOV AH,03H
-	INT 10H
-	INC DL
-	MOV AH,02H
-	INT 10H
-	POP CX
-	
-	MOV DL,'-'
-	MOV AH,06H
-	INT 21H
-	
-	XOR AH,AH
-	MOV AL,CODSYM
-	DIV CONST16
-	MOV BX,OFFSET HEXSYM
-	XLAT
-	XCHG AX,DX
-	MOV AH,06H
-	INT 21H
-	
-	MOV AL,DH
-	XLAT
-	MOV DL,AL
-	MOV AH,06H
-	INT 21H
-	
-	MOV DL,' '
-	MOV AH,06H
-	INT 21H
+ .MODEL SMALL
+ .STACK 100H
 
-	ADD CODSYM,1
-	LOOP PRINT_SYMBOL
-	
-	INT 20H
-END
+ .DATA
+    CONST16 DB 16
+    HEXSYM DB '0123456789ABCDEF'
+    CODSYM DB 0
+ .CODE
+   MAIN PROC
+     MOV AX, @DATA                 
+     MOV DS, AX
+
+     MOV CX, 256                  
+     
+     MOV AH, 2                     
+     MOV DX, 256     
+     O:
+        MOV DX, 256
+        SUB DX, CX   
+        MOV AX,40H
+        MOV ES,AX
+        MOV BH,ES:[62H]
+        MOV AH,0AH
+        MOV AL,CODSYM
+        MOV DX, CX
+        MOV SI, CX
+        MOV CX,1
+        INT 10H
+        MOV AH,03H
+        INT 10H
+        INC DL
+        MOV AH,02H
+        INT 10H
+        MOV DL,'-'
+        MOV AH,06H
+        INT 21H
+        MOV CX, SI                         
+        XOR AH,AH
+        MOV AL,CODSYM
+        DIV CONST16
+        MOV BX,OFFSET HEXSYM
+        XLAT
+        XCHG AX,DX
+        MOV AH,06H
+        INT 21H
+        
+        MOV AL,DH
+        XLAT
+        MOV DL,AL
+        MOV AH,06H
+        INT 21H
+    
+        MOV DL,' '
+        MOV AH,06H
+        INT 21H
+        ADD CODSYM,1
+        LOOP O                   
+
+     MOV AH, 4CH                  
+     INT 21H
+   MAIN ENDP
+ END MAIN
